53f0d18eeaa7cbb8d1aac614d9e184395e4621ae
REYAN YADAV <107745747+REYAN-YADAV@users.noreply.github.com>
2022-12-24 00:04:41 +0530

    conditional_satatement

---
+++ while_ex.py
@@ -0,0 +1,45 @@
+''' # WHILE LOOP
+       ************   
+# example 1. print out 10 times Welcome to while loop.
+
+i=1
+while i<=10:
+	print("Welcome to while loop")
+	i=i+1     
+
+# example 2. print out 1 to 10.
+
+i=1
+while i<=10:
+	print(i)
+	i=i+1    
+
+#example 3. to print number from 1 to 20 which divisible by 3.
+
+x=1
+while x<=20:
+	if x%3==0:
+		print(x)
+	x=x+1        
+
+#example 4. to print the sum of first n number.
+
+n = int(input("Enter a natural number: "))
+sum=0
+i=1
+while i<=n:
+	sum=sum+i;
+	i=i+1
+print("The sum: ",sum)  '''
+
+# example 5. To print your favourite actress on my condition.
+
+name = " "
+while name!="natasha malkova":
+	name = input("your favourite actress: ")
+print("Thanks for confirmaton")
+
+
+	
+
+
